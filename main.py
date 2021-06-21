0488342dcebdf444e031d569329f19a4b14f5c4e
John Atwood <jsa5593@rit.edu>
2021-06-21 17:50:02 -0400

    added API wrapper start

---
+++ main.py
@@ -0,0 +1,69 @@
+import argparse
+import requests
+
+URL = 'https://qvthkmtukh.execute-api.us-west-2.amazonaws.com/master'
+PARAMETERS = {
+        "x-api-key": 'nDbo6sUAbM2DaNk60N7Ft1moG7OUOSfz1Os0QlR2'
+    }
+
+
+def api():
+    print('API')
+
+
+def account_info():
+    response = requests.get(URL + '/api/v1/account/info', headers=PARAMETERS)
+    print(response.json())
+
+
+def all_files(att, limit):
+    if att == '' and limit == '':
+        response = requests.get(URL + '/api/v1/files', headers=PARAMETERS)
+        print(response.json())
+    elif att != '' and limit != '':
+        response = requests.get(URL + '/api/v1/files?limit=' + args.limit + '&attributes=' + args.attributes,
+                                headers=PARAMETERS)
+        print(response.json())
+    elif att == '':
+        response = requests.get(URL + '/api/v1/files?limit=' + args.limit, headers=PARAMETERS)
+        print(response.json())
+    else:
+        response = requests.get(URL + '/api/v1/files?attributes=' + args.attributes, headers=PARAMETERS)
+        print(response.json())
+
+
+def file_by_id(id):
+    response = requests.get(URL + '/api/v1/files/' + id, headers=PARAMETERS)
+    print(response.json())
+
+
+def devices():
+    response = requests.get(URL + '/api/v1/devices/', headers=PARAMETERS)
+    print(response.json())
+
+
+def device_by_id(id):
+    response = requests.get(URL + '/api/v1/devices/' + id, headers=PARAMETERS)
+    print(response.json())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=['files', 'file-id', 'devices', 'device-id', 'account', 'attributes'])
+    parser.add_argument('--id', '-i', default='')
+    parser.add_argument('--attributes', '-a', default='')
+    parser.add_argument('--limit', '-l', default='')
+    args = parser.parse_args()
+
+    if args.command == 'account':
+        account_info()
+    elif args.command == 'files':
+        all_files(args.attributes, args.limit)
+    elif args.command == 'file-id':
+        file_by_id(args.id)
+    elif args.command == 'devices':
+        devices()
+    elif args.command == 'device-id':
+        device_by_id(args.id)
+    elif args.command == 'attributes':
+        api()
