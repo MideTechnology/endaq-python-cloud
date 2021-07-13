8aaf3c8a270d65e68011ee42a052e40c963c9193
MIDE\jatwood <jatwood@hutchinsoninc.com>
2021-07-13 12:21:43 -0400

    added verbose output

--- endaq/cloud/API_wrapper.py
+++ endaq/cloud/API_wrapper.py
@@ -30,14 +30,15 @@ def attributes_file(data, output_path):
                 csv_writer.writerow([x['id'], y])
 
 
-def account_info():
+def account_info(verbose):
     """Request account info from the cloud and print the results."""
-    print('GET', URL + '/api/v1/account/info')
+    if verbose:
+        print('GET', URL + '/api/v1/account/info')
     response = requests.get(URL + '/api/v1/account/info', headers=PARAMETERS)
     print(response.json())
 
 
-def all_files(att, limit, output_path):
+def all_files(att, limit, output_path, verbose):
     """
     Request file data from the cloud and write the output to a file.
 
@@ -48,7 +49,8 @@ def all_files(att, limit, output_path):
     query_data = {k: v for k, v in dict(limit=limit, attributes=att).items() if v}
     query = ('?' + urllib.parse.urlencode(query_data)) if query_data else ''
 
-    print('GET', URL + '/api/v1/files' + query)
+    if verbose:
+        print('GET', URL + '/api/v1/files' + query)
     response = requests.get(URL + '/api/v1/files' + query, headers=PARAMETERS)
     data = response.json()['data']
     if att:
@@ -64,14 +66,15 @@ def all_files(att, limit, output_path):
             csv_writer.writerow(x.values())
 
 
-def file_by_id(id_, output_path):
+def file_by_id(id_, output_path, verbose):
     """
     Request a specific file's data from the cloud and write the output to files.
 
     :param id_: File ID
     :param output_path: Output directory
     """
-    print('GET', URL + '/api/v1/files/' + id_)
+    if verbose:
+        print('GET', URL + '/api/v1/files/' + id_)
     response = requests.get(URL + '/api/v1/files/' + id_, headers=PARAMETERS)
     data = response.json()
 
@@ -90,14 +93,15 @@ def file_by_id(id_, output_path):
         csv_writer.writerow(data.values())
 
 
-def devices(output_path, limit):
+def devices(output_path, limit, verbose):
     """
     Request devices' data from the cloud and write the output to a file.
 
     :param output_path: output file location
     :param limit: number of devices shown
     """
-    print('GET', URL + '/api/v1/devices/')
+    if verbose:
+        print('GET', URL + '/api/v1/devices/')
     if limit == '':
         response = requests.get(URL + '/api/v1/devices/', headers=PARAMETERS)
     else:
@@ -112,14 +116,15 @@ def devices(output_path, limit):
             csv_writer.writerow(x.values())
 
 
-def device_by_id(id_, output_path):
+def device_by_id(id_, output_path, verbose):
     """
     Request a specific device's data from the cloud and write the output to a file.
     
     :param id_: ID of specific device
     :param output_path: output location for information
     """
-    print('GET', URL + '/api/v1/devices/' + id_)
+    if verbose:
+        print('GET', URL + '/api/v1/devices/' + id_)
     response = requests.get(URL + '/api/v1/devices/' + id_, headers=PARAMETERS)
 
     data = response.json()
@@ -130,7 +135,7 @@ def device_by_id(id_, output_path):
     csv_writer.writerow(data.values())
 
 
-def post_attribute(id_, name, type_, value):
+def post_attribute(id_, name, type_, value, verbose):
     """
     Post attribute data to the cloud and print the results.
 
@@ -139,7 +144,8 @@ def post_attribute(id_, name, type_, value):
     :param type_: attribute type
     :param value: attribute value
     """
-    print('POST', URL + '/api/v1/attributes')
+    if verbose:
+        print('POST', URL + '/api/v1/attributes')
     attributes = {"name": name,
                   "type": type_,
                   "value": value,
@@ -148,17 +154,23 @@ def post_attribute(id_, name, type_, value):
     print(att.json())
 
 
+def make_env(key):
+    with open('.env', 'w') as f:
+        f.write(f'API_KEY={key}')
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('command', choices=['files', 'file-id', 'devices', 'device-id', 'account', 'attribute'])
+    parser.add_argument('command', choices=['files', 'file-id', 'devices', 'device-id', 'account', 'attribute', 'set-env'])
     parser.add_argument('--id', '-i', default='', help='Device or File id')
     parser.add_argument('--attributes', '-a', default='', help='What attributes you want to view; default is none, '
                                                                'can be all, or att1,att2,...')
     parser.add_argument('--limit', '-l', default='', help='How many devices or files you want returned; Max 100')
-    parser.add_argument('--key', '-k', default='', help='API key if it is not in the .env file')
+    parser.add_argument('--key', '-k', default='', help='API key if it is not in the .env file or you want to set env')
     parser.add_argument('--name', '-n', help='Name of new attribute')
     parser.add_argument('--type', '-t', help='Type of element the new attribute is')
     parser.add_argument('--value', '-v', help='Value of new attribute')
+    parser.add_argument('--verbose', '-V', action='store_true', help='Shows API url calls')
 
     parser.add_argument('--url', '-u', default='', help=argparse.SUPPRESS)
     parser.add_argument('--output', '-o', default='')
@@ -181,24 +193,29 @@ def main():
         sys.exit('Create a .env with API Key or pass it in with --key')
 
     if args.command == 'account':
-        account_info()
+        account_info(args.verbose)
     elif args.command == 'files':
-        all_files(args.attributes, args.limit, output)
+        all_files(args.attributes, args.limit, output, args.verbose)
         print('output can be found in output/files.csv and output/attributes.csv')
     elif args.command == 'file-id' and args.id != '':
-        file_by_id(args.id, output)
+        file_by_id(args.id, output, args.verbose)
         print(f'output can be found in output/file_{args.id}.csv and output/attributes.csv')
     elif args.command == 'devices':
-        devices(output, args.limit)
+        devices(output, args.limit, args.verbose)
         print('output can be found in devices.csv')
     elif args.command == 'device-id' and args.id != '':
-        device_by_id(args.id, output)
+        device_by_id(args.id, output, args.verbose)
         print('output can be found in devices.csv')
     elif args.command == 'attribute':
         if args.name is not None and args.type is not None and args.value is not None and args.id is not None:
-            post_attribute(args.id, args.name, args.type, args.value)
+            post_attribute(args.id, args.name, args.type, args.value, args.verbose)
         else:
             sys.exit('id, name, type, and value can\'t be empty')
+    elif args.command == 'set-env':
+        if args.key == '':
+            sys.exit('Need -k or --key for this command')
+        else:
+            make_env(args.key)
     else:
         sys.exit('Use -h for help with commands.')
 
